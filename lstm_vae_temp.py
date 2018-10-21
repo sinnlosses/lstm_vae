d19282150696b3f5f4279742f0eb204048f0e636
sinnlosses <fujiyoshiyuki@gmail.com>
2018-10-21 18:36:23 +0900

    lstm_vae_complete

--- lstm_vae_temp.py
+++ lstm_vae_temp.py
@@ -1,45 +1,43 @@
 # coding: utf-8
 
+import os
+import re
+import random
+import numpy as np
+import pickle
+
+os.environ["CUDA_DEVICE_ORDER"] = 'PCI_BUS_ID'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+
 from keras import backend as K
 from keras import objectives
 from keras.layers import Input, LSTM, Embedding
 from keras.layers.core import Dense, Lambda
 from keras.layers.wrappers import TimeDistributed
-from keras.models import Model
+from keras.models import Model, model_from_json
+from keras.callbacks import LambdaCallback, ModelCheckpoint, EarlyStopping
+from keras.utils import plot_model
+
+from utils import sent_to_surface_conjugated, max_sent_len, save_config
+from utils import create_words_set, create_vae_ex_dx_y, create_emb_and_dump
+from utils import plot_history_loss, choise_output_word_id, add_funcword
+from utils import lstm_predict_sampling, add_sample, inference
 
 def on_epoch_end(epoch, logs):
-    BorEOS = ""
     print('----- Generating text after Epoch: %d' % epoch)
-    x_pred = np.zeros(shape=(1,maxlen),dtype='int32')
-    x_pred[0,0] = word_to_id[BorEOS]
-    z_pred = np.random.normal(0,1,(1,intermediate_dim))
-    sentence = []
-    for i in range(maxlen-1):
-        preds = gen.predict([x_pred,z_pred,z_pred], verbose=0)[0]
-        output_id = choise_output_word_id(preds[i], mode="greedy")
-        output_word = id_to_word[output_id]
-        sentence.append(output_word)
-        if output_word == BorEOS:
-            break
-        x_pred[0,i+1] = output_id
-    if sentence[-1] != BorEOS:
-        err_mes = "produce_failed!"
-        print(err_mes)
-        return
-
-    del sentence[-1]
-    sent_surface = [w_m.split("_")[0] for w_m in sentence]
-    sent_surface = " ".join(sent_surface)
+    sent_surface, _ = inference(gen,
+                             maxlen,
+                             latent_dim,
+                             word_to_id,
+                             id_to_word)
     print(sent_surface)
-
     return
 
 def create_lstm_vae(maxlen:int,
                     batch_size:int,  # we need it for sampling
                     intermediate_dim:int,
                     latent_dim:int,
-                    embedding_matrix,
-                    ):
+                    embedding_matrix):
     """
     Creates an LSTM Variational Autoencoder (VAE).
 
@@ -52,20 +50,22 @@ def create_lstm_vae(maxlen:int,
     """
     words_num = embedding_matrix.shape[0]
     w2v_dim = embedding_matrix.shape[1]
-    x = Input(shape=(maxlen,), dtype='int32', name='main_input')
+    x = Input(shape=(maxlen,), dtype='int32', name='enc_input')
     emb = Embedding(input_dim=words_num,
                     output_dim=w2v_dim,
                     input_length=maxlen,
                     weights=[embedding_matrix],
                     mask_zero=True,
-                    trainable=True)
+                    trainable=True,
+                    name='emb')
     x_embed = emb(x)
     # LSTM encoding
     h = LSTM(units=intermediate_dim,
-             return_sequences=False)(emb)
+             return_sequences=False,
+             name='enc_lstm')(x_embed)
     # VAE Z layer
-    z_mean = Dense(units=latent_dim)(h)
-    z_log_sigma = Dense(units=latent_dim)(h)
+    z_mean = Dense(units=latent_dim, name='z_mean')(h)
+    z_log_sigma = Dense(units=latent_dim, name='z_log_sigma')(h)
 
     def sampling(args):
         z_mean, z_log_sigma = args
@@ -74,23 +74,25 @@ def create_lstm_vae(maxlen:int,
 
     # note that "output_shape" isn't necessary with the TensorFlow backend
     # so you could write `Lambda(sampling)([z_mean, z_log_sigma])`
-    z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_sigma])
+    z = Lambda(sampling, name='z')([z_mean, z_log_sigma])
 
     z_reweighting = Dense(units=intermediate_dim, activation="linear")
     z_reweighted = z_reweighting(z)
 
     # "next-word" data for prediction
-    decoder_words_input = Input(shape=(maxlen,))
+    decoder_words_input = Input(shape=(maxlen,),
+                                dtype='int32',
+                                name='dec_input')
     decoded_x_embed = emb(decoder_words_input)
     # decoded LSTM layer
     decoder_h = LSTM(intermediate_dim,
                      return_sequences=True,
-                     return_state=False)
+                     name='dec_lstm')
 
     # todo: not sure if this initialization is correct
     h_decoded = decoder_h(decoded_x_embed,
                         initial_state=[z_reweighted, z_reweighted])
-    decoder_dense = TimeDistributed(Dense(words_num+1,
+    decoder_dense = TimeDistributed(Dense(words_num,
                                           activation="softmax",
                                           name='main_output'))
     decoded_onehot = decoder_dense(h_decoded)
@@ -104,7 +106,7 @@ def create_lstm_vae(maxlen:int,
     # generator, from latent space to reconstructed inputs -- for inference's first step
     decoder_state_input = Input(shape=(latent_dim,))
     _z_reweighted = z_reweighting(decoder_state_input)
-    _h_decoded = decoder_h(decoder_words_input,
+    _h_decoded = decoder_h(decoded_x_embed,
                            initial_state=[_z_reweighted, _z_reweighted])
     _decoded_onehot = decoder_dense(_h_decoded)
     generator = Model([decoder_words_input,decoder_state_input],
@@ -117,21 +119,170 @@ def create_lstm_vae(maxlen:int,
         return loss
 
     vae.compile(optimizer="adam", loss=vae_loss)
-    vae.summary()
-
     return vae, encoder, generator
 
+def data_check():
+    # コピーのソースの確認
+    if not os.path.exists(data_fname):
+        raise IOError(f"{data_fname}がありません")
+    # 各モデルを保存するベースDirの確認
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
+    # モデルのDirの確認
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+    # 重みを保存するDirの確認
+    if not os.path.exists(weights_dir):
+        os.mkdir(weights_dir)
+    if is_lang_model:
+        if not os.path.exists(func_wordsets_fname):
+            raise IOError(f"{func_wordsets_fname}がありません")
+    if is_data_analyzed:
+        if not os.path.exists(save_data_fname):
+            raise IOError("保存されたanalyzed dataがありません")
+
+    return
+
 if __name__ == '__main__':
+
+    data_fname = "/Users/fy/Downloads/copy/python_lab/keras/lstm/source/copy_temp.txt"
+    # data_fname = "./source/wiki_edojidai.txt"
+    # base_dir = "templete_model"
+    base_dir = "templete_model"
+    # model_dir_name = "models_5000"
+    model_dir_name = "model_temp"
+    func_wordsets_fname = "func_wordsets.p"
+    w2v_fname = "/Users/fy/Downloads/copy/python_lab/keras/lstm/model.bin"
     maxlen = 40
+    mecab_lv = 3
+    save_weight_period = 20
+    epochs = 10
     batch_size = 32
     intermediate_dim = 64
-    latent_dim = 50
-    words_num = 1000
-    embedding_matrix =
-    vae, encoder, generator = create_lstm_vae(
-                                maxlen,
-                                batch_size,
-                                intermediate_dim,
-                                latent_dim,
-                                words_num,
-                                embedding_matrix=None)
+    latent_dim = 32
+    is_data_analyzed = False
+    is_lang_model = False
+    is_reversed = True
+    use_loaded_emb = True
+    use_loaded_model = False
+    use_loaded_weight = False
+    use_conjugated = True
+
+    model_dir = os.path.join(base_dir,model_dir_name)
+    weights_dir = os.path.join(model_dir,"weights")
+    w2v_emb_fname = os.path.join(model_dir, "emb_wordsets.p")
+    save_w2i_fname = os.path.join(model_dir, "word2id.p")
+    save_data_fname = os.path.join(model_dir, "analyzed_data.txt")
+    save_model_fname = os.path.join(model_dir, "model.json")
+    save_weights_fname = os.path.join(weights_dir, "weights.hdf5")
+    save_callback_weights_fname = os.path.join(weights_dir,"weights_{epoch:03d}_{loss:.2f}.hdf5")
+    save_config_fname = os.path.join(model_dir, "config.txt")
+    save_loss_fname = os.path.join(model_dir, "loss.png")
+    data_check()
+
+    # 解析済みデータをロードするならここは必要ない
+    if is_data_analyzed:
+        with open(save_data_fname,"r") as fi:
+            sent_list = fi.readlines()
+        print("解析済みデータをロードしました")
+    else:
+        print("データを解析します")
+        with open(data_fname, "r") as fi:
+            data = fi.read()
+        sent_list = sent_to_surface_conjugated(
+                        data,
+                        save_path=save_data_fname,
+                        level=mecab_lv,
+                        use_conjugated=use_conjugated)
+    sent_list = [sent.strip() for sent in sent_list if 3 <= len(sent.split(" ")) <= maxlen]
+    if len(sent_list) % batch_size != 0:
+        sent_list = add_sample(sent_list, batch_size)
+
+    # 各種データの情報
+    n_samples = len(sent_list)
+    maxlen = max_sent_len(sent_list) + 1
+    words_set = create_words_set(sent_list)
+    if is_lang_model:
+        print("機能語のセットをロードします")
+        with open(func_wordsets_fname,"rb") as fi:
+            funcwords_set = pickle.load(fi)
+        words_set = sorted(words_set | funcwords_set)
+    words_num = len(words_set)
+
+    print("入出力データを作成中")
+    X_enc, X_dec, Y, word_to_id = create_vae_ex_dx_y(
+                                        sent_list,
+                                        maxlen,
+                                        words_num,
+                                        is_reversed=is_reversed)
+    if is_lang_model:
+        word_to_id = add_funcword(word_to_id, funcwords_set)
+        if words_num != len(word_to_id):
+            print(words_num, len(word_to_id))
+            raise AssertionError("words_numとword_to_idの総数が異なります")
+
+    if use_loaded_emb:
+        print("保存されたembをロードします")
+        if not os.path.exists(path=w2v_emb_fname):
+            raise IOError("w2vのファイルがありません")
+        with open(w2v_emb_fname, "rb") as fi:
+            embedding_matrix, loaded_words_set = pickle.load(fi)
+        if words_set != loaded_words_set:
+            raise ValueError("words_setに含まれる単語が一致しません")
+    else:
+        print("embを作成し、保存します")
+        embedding_matrix = create_emb_and_dump(w2v_fname,
+                                               words_set,
+                                               word_to_id,
+                                               w2v_emb_fname)
+
+    w2v_dim = len(embedding_matrix[1])
+    id_to_word = {i:w for w,i in word_to_id.items()}
+    if use_loaded_model:
+        print("保存されたモデルをロードします")
+        with open(save_model_fname,"r") as fi:
+            json_string = fi.read()
+            vae, enc, gen = model_from_json(json_string)
+    else:
+        print("モデルを構築します")
+        vae, enc, gen = create_lstm_vae(
+                            maxlen=maxlen,
+                            batch_size=batch_size,
+                            intermediate_dim=intermediate_dim,
+                            latent_dim=latent_dim,
+                            embedding_matrix=embedding_matrix)
+    if use_loaded_weight:
+        print("重みをロードしました")
+        vae = vae.load_weights(save_weights_fname)
+
+    vae.summary()
+    # gen.summary()
+    save_dict = {"n_samples":str(n_samples),
+                 "maxlen":str(maxlen),
+                 "words_num":str(words_num),
+                 "intermediate_dim":str(intermediate_dim),
+                 "w2v_dim":str(w2v_dim),
+                 "is_reversed":str(is_reversed),
+                 "mecab_lv":str(mecab_lv),
+                 "use_conjugated":str(use_conjugated)
+                 }
+    save_config(path=save_config_fname, save_dict=save_dict)
+    with open(save_w2i_fname, "wb") as fo:
+        pickle.dump([word_to_id, is_reversed], fo)
+
+    print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+    model_checkpoint = ModelCheckpoint(filepath=save_callback_weights_fname,
+                                        save_weights_only=True,
+                                        period=save_weight_period)
+
+    print("Training model...")
+    fit = vae.fit([X_enc, X_dec],
+             Y,
+             epochs=epochs,
+             verbose=1,
+             batch_size=batch_size,
+             callbacks=[print_callback, model_checkpoint])
+    loss_history = fit.history['loss']
+    gen.save_weights(save_weights_fname)
+    plot_history_loss(save_loss_fname,loss_history)
+    import pdb; pdb.set_trace()
---
+++ utils.py
@@ -0,0 +1,283 @@
+
+import os
+import numpy as np
+import MeCab
+import pickle
+import random
+
+from keras.preprocessing import sequence
+from keras.preprocessing.text import text_to_word_sequence, Tokenizer
+from keras.utils import to_categorical
+from gensim.models import KeyedVectors
+import matplotlib.pyplot as plt
+
+def sent_to_surface_conjugated(sentences:str,
+                               save_path:str,
+                               level:int=4,
+                               use_conjugated:bool=True):
+    """
+        \nで区切られた各文をmecabで解析し表層形+levelで決めた数の情報を
+        アンダースコアでつなげて保存する
+    """
+    mecab = MeCab.Tagger()
+    mecab.parse("")
+    sent_list = sentences.split("\n")
+    mecabed_list = []
+    for sent in sent_list:
+        word_morph_list = []
+        node = mecab.parseToNode(sent)
+        while node:
+            morphes = node.feature.split(",")
+            if morphes[0] == "BOS/EOS":
+                wm = "<BOS/EOS>_BOS/EOS".lower()
+                word_morph_list.append(wm)
+                node = node.next
+                continue
+            word = node.surface
+            tail_infoes = morphes[:level]
+            if use_conjugated:
+                tail_infoes.append(morphes[5])
+            tail_infoes = "_".join(tail_infoes)
+            wm = f"{word}_{tail_infoes}"
+            word_morph_list.append(wm)
+            node = node.next
+        result = " ".join(word_morph_list)
+        mecabed_list.append(result)
+
+    with open(save_path, "w") as fo:
+        fo.write("\n".join(mecabed_list))
+    return mecabed_list
+
+def max_sent_len(sent_list:list):
+    return max([len(sent.split(" ")) for sent in sent_list])
+
+def save_config(path:str, save_dict:dict):
+    """
+        sava_config
+    """
+    result = [f"{k} = {v}" for k,v in save_dict.items()]
+    with open(path,"w") as fo:
+        fo.write("Config\n")
+        fo.write("\n".join(result))
+    return
+
+def create_words_set(sent_list:list):
+    sentences = " ".join(sent_list)
+    words_sequence = text_to_word_sequence(text=sentences,
+                                      filters='\n',
+                                      split=" ")
+    return set(sorted(words_sequence))
+
+def create_vae_ex_dx_y(
+                    sent_list:list,
+                    maxlen:int,
+                    words_num:int,
+                    is_reversed:bool=False):
+    """VAEに必要なEncoder X, Decoder X, Yを出力する
+        sent_list: リスト
+        maxlen: 最大長
+        words_num: 単語の総数
+        is_reversed: 逆順かどうか
+    """
+    n_samples = len(sent_list)
+    tokenizer = Tokenizer(filters='\n')
+    tokenizer.fit_on_texts(sent_list)
+    sent_seq = tokenizer.texts_to_sequences(sent_list)
+    word_to_id = tokenizer.word_index
+
+    if is_reversed:
+        X_enc = [sent[-2:0:-1] for sent in sent_seq]
+        X_dec = [sent[-1:0:-1] for sent in sent_seq]
+        y_seq = [sent[-2::-1] for sent in sent_seq]
+    else:
+        X_enc = [sent[1:-1] for sent in sent_seq]
+        X_dec = [sent[:-1] for sent in sent_seq]
+        y_seq = [sent[1:] for sent in sent_seq]
+
+    X_enc = sequence.pad_sequences(sequences=X_enc,
+                               maxlen=maxlen,
+                               padding='post')
+    X_dec = sequence.pad_sequences(sequences=X_dec,
+                               maxlen=maxlen,
+                               padding='post')
+    y_seq = sequence.pad_sequences(sequences=y_seq,
+                                   maxlen=maxlen,
+                                   padding='post')
+    Y = np.zeros((n_samples, maxlen, words_num+1), dtype=np.bool)
+    for i, id_seq in enumerate(y_seq):
+        for j, id in enumerate(id_seq):
+            if id == 0:
+                continue
+            Y[i,j,id] = 1.
+
+    # with open(fname, "wb") as fo:
+    #     pickle.dump([X,Y,word_to_id],fo)
+    return X_enc, X_dec, Y, word_to_id
+
+def create_emb_and_dump(w2v_fname,
+                        words_set,
+                        word_to_id,
+                        fname="emb_wordsets.p"):
+
+    print("w2vデータをload中...")
+    words_num = len(words_set)
+    w2v = KeyedVectors.load_word2vec_format(w2v_fname, binary=True)
+    w2v_dim = w2v.vector_size
+    embedding_matrix = np.zeros((words_num+1, w2v_dim))
+    for word in words_set:
+        word_surface = word.split("_")[0]
+        id = word_to_id[word]
+        if word_surface not in w2v.vocab:
+            embedding_matrix[id] = np.random.normal(0,1,(1,w2v_dim))
+        else:
+            embedding_matrix[id] = w2v.wv[word_surface]
+    with open(fname,"wb") as fo:
+        pickle.dump([embedding_matrix, words_set],fo)
+
+    return embedding_matrix
+
+def plot_history_loss(save_path:str,
+                      loss_history:list,
+                      val_loss_history=None):
+    # Plot the loss in the history
+    fig = plt.figure(1)
+    plt.plot(loss_history,label="loss for training")
+    if val_loss_history is not None:
+        plt.plot(val_loss_history,label="loss for validation")
+    plt.title("model_loss")
+    plt.xlabel("epoch")
+    plt.ylabel('loss: cross_entropy')
+    plt.legend(loc='upper right')
+    fig.savefig(save_path)
+
+    return
+
+def choise_output_word_id(distribution, mode='greedy'):
+    BorEOS = "<BOS/EOS>_BOS/EOS".lower()
+    output_ids = np.argsort(distribution)[::-1]
+    def check(id):
+        if id == 0:
+            return False
+        return True
+
+    if mode == "greedy":
+        i = 0
+        while True:
+            output_id = output_ids[i]
+            if output_id != 0:
+                break
+            else:
+                i += 1
+    elif mode == "random":
+        output_ids = output_ids[:3]
+        while True:
+            output_id = random.choice(output_ids)
+            if output_id != 0:
+                break
+    else:
+        raise ValueError("modeの値が間違っています")
+
+    return output_id
+
+def add_funcword(word_to_id:dict, funcwords_set:set):
+    total_ids = len(word_to_id)
+    for func_word in funcwords_set:
+        if func_word not in word_to_id:
+            total_ids += 1
+            word_to_id[func_word] = total_ids
+    return word_to_id
+
+def printing_sample(csv:str, save_path:str="temp.txt"):
+    with open(csv, "r") as fi:
+        sent_list = fi.readlines()
+
+    sent_list = [sent.strip().split(",")[0] for sent in sent_list]
+    with open(save_path, "w") as fo:
+        fo.write("\n".join(sent_list))
+
+def lstm_predict_sampling(model,
+                     maxlen:int,
+                     word_to_id:dict,
+                     id_to_word:dict,
+                     h_length:int,
+                     is_reversed:bool):
+
+    BorEOS = "<BOS/EOS>_BOS/EOS".lower()
+    x_pred = np.zeros(shape=(1,maxlen),dtype='int32')
+    x_pred[0,0] = word_to_id[BorEOS]
+    h_pred = np.random.normal(0,1,(1,h_length))
+    c_pred = np.random.normal(0,1,(1,h_length))
+    sentence = []
+    for i in range(maxlen-1):
+        preds = model.predict([x_pred,h_pred,c_pred], verbose=0)[0]
+        output_id = choise_output_word_id(preds[i], mode="greedy")
+        output_word = id_to_word[output_id]
+        sentence.append(output_word)
+        if output_word == BorEOS:
+            break
+        x_pred[0,i+1] = output_id
+    if sentence[-1] != BorEOS:
+        err_mes = "produce_failed!"
+        print(err_mes)
+
+    del sentence[-1]
+    if not sentence:
+        err_mes = "white"
+        print(err_mes)
+
+    sent_surface = [w_m.split("_")[0] for w_m in sentence]
+    if is_reversed:
+        sent_surface = [word for word in reversed(sent_surface)]
+    sent_surface = " ".join(sent_surface)
+    sent_morph = [create_sent_morph(w_m) for w_m in sentence]
+    sent_morph = " ".join(sent_morph)
+
+    return (sent_surface, sent_morph)
+
+def inference(gen_model,
+              maxlen:int,
+              latent_dim:int,
+              word_to_id:dict,
+              id_to_word:dict):
+    BorEOS = "<BOS/EOS>_BOS/EOS".lower()
+    x_pred = np.zeros(shape=(1,maxlen),dtype='int32')
+    x_pred[0,0] = word_to_id[BorEOS]
+    z_pred = np.random.normal(0,1,(1,latent_dim))
+    sentence = []
+    for i in range(maxlen-1):
+        preds = gen_model.predict([x_pred,z_pred], verbose=0)[0]
+        output_id = choise_output_word_id(preds[i], mode="greedy")
+        output_word = id_to_word[output_id]
+        sentence.append(output_word)
+        if output_word == BorEOS:
+            break
+        x_pred[0,i+1] = output_id
+    if sentence[-1] != BorEOS:
+        err_mes = "produce_failed!"
+        return err_mes, None
+
+    del sentence[-1]
+    sent_surface = [w_m.split("_")[0] for w_m in sentence]
+    sent_surface = " ".join(sent_surface)
+    sent_morph = [create_sent_morph(w_m) for w_m in sentence]
+    sent_morph = " ".join(sent_morph)
+
+    return sent_surface, sent_morph
+
+def create_sent_morph(w_m):
+    word, morph = w_m.split("_")[0], w_m.split("_")[1]
+    if morph in ["助詞","助動詞","記号","接続詞"]:
+        res = w_m
+    else:
+        m = w_m.split("_")[1:]
+        m = "_".join(m)
+        res = "<{}>".format(m)
+    return res
+
+def add_sample(sent_list:list, batch_size:int):
+    amari = len(sent_list) % batch_size
+    num_adds = batch_size - amari
+    adds = random.sample(sent_list, k=num_adds)
+    sent_list.extend(adds)
+
+    return sent_list
